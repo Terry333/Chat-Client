f85aa1955147cd5e56472adc914d6dceebee34ff
Terry <Terry333@protonmail.com>
2020-01-26 14:41:42 -0600

    commit 1

--- ChatApp/Classes/Client.cs
+++ ChatApp/Classes/Client.cs
@@ -5,6 +5,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
+using System.Windows.Controls;
 
 namespace ChatApp.Classes
 {
@@ -17,20 +18,24 @@ namespace ChatApp.Classes
 
         private RSACryptoServiceProvider rsa;
         private NetworkStream stream;
-        private string key;
+        private string publicKey;
+        private string privateKey;
 
-        public Client(string server, int port, String username, string key)
+        public Client(string server, int port, String username)
         {
             Server = server;
             Port = port;
             UserName = username;
             rsa = new RSACryptoServiceProvider();
-            this.key = key;
+            publicKey = rsa.ToXmlString(false);
+            privateKey = rsa.ToXmlString(true);
             try
             {
                 ChatClient = new TcpClient(Server, Port);
-                Byte[] data = System.Text.Encoding.Unicode.GetBytes("Client Connected 👋: " + UserName);
+                Byte[] data = Encrypt("Client Connected 👋: " + UserName);
                 stream = ChatClient.GetStream();
+                stream.Write(data, 0, data.Length);
+                data = Encoding.ASCII.GetBytes(publicKey);
                 stream.Write(data, 0, data.Length);
             }
             catch
@@ -39,15 +44,45 @@ namespace ChatApp.Classes
             }
         }
 
-        private string GenerateKey()
+        private byte[] Encrypt(String message)
         {
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            Byte[] data = encoding.GetBytes(message);
+            return rsa.Encrypt(data, false);
+        }
 
+        private String Decrypt(Byte[] data)
+        {
+            byte[] decryptedData = rsa.Decrypt(data, false);
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            return encoding.GetString(decryptedData);
         }
 
         public bool SendMessage(String message)
         {
+            try
+            {
+                Byte[] data = Encrypt(message);
+                stream.Write(data, 0, data.Length);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
 
-            return false;
+        public void ReceiverMethod(TextBlock console)
+        {
+            Byte[] data = new Byte[256];
+            Int32 bytes;
+            String message = String.Empty;
+            for(; ; )
+            {
+                bytes = stream.Read(data, 0, data.Length);
+                message = Decrypt(data);
+                console.Text = console.Text + "\n" + message;
+            }
         }
 
         public bool Close()
